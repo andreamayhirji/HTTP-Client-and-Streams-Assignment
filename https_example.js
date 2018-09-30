1d7bcfd3f0ec27153d7548797c20c2ce2a6f4d03
Andrea <andreamayhirji@gmail.com>
2018-09-30 23:13:20 +0000

    added a request for www.example.org

--- https_example.js
+++ https_example.js
@@ -1,8 +1,24 @@
-/* 
-Require the https module
-In your presently empty https_example.js file, require the https module:
-*/
-
+ 
+// 1. Require the https module
 var https = require('https');
 
-console.log('I did it!');
+// console.log('I did it!');
+
+// Make a simple request to example.org
+var options = {
+    host: 'www.example.org',
+    path: '/'
+};
+
+//called by https when the request is made.
+
+var callback = function () {
+    console.log('In response handler callback!');
+}
+console.log("I'm about to make the request...");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
+
+
