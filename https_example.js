25e1918c3a715ea463c2def8e55fc1203b4967a3
Andrea <andreamayhirji@gmail.com>
2018-09-30 23:17:45 +0000

    revised the callback function to include the response.

--- https_example.js
+++ https_example.js
@@ -12,9 +12,11 @@ var options = {
 
 //called by https when the request is made.
 
-var callback = function () {
-    console.log('In response handler callback!');
+var callback = function (response) {
+    console.log("In response handler callback!");
+    console.log('Response: ', response);
 }
+
 console.log("I'm about to make the request...");
 
 https.request(options, callback).end();
